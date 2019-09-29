7bdfcae5cc9e398a7c5e70e4eceaf3fd5208920f
brunogeek9 <brunogeek9@gmail.com>
2019-09-29 10:28:45 -0300

    atualização nas rotas

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,9 @@ const express = require('express');
 const routes = express.Router();
 const crud = require('./mongo_cruds/crud_clock');
 routes.get('/clocks', crud.index);
-routes.post('/clocks', async (request, response) => {
-    try {
-        const clock = await Clock.create(request.body);
-        response.send(clock);
-    } catch (error) {
-        response.status(500).send(error);
-    }
-});
+routes.post('/clocks', crud.store);
 routes.get('/clock/:id',crud.show);
-routes.put('/clock',crud.update);
+routes.put('/clock/:id',crud.update);
 routes.delete('/clock/:id',crud.destroy);
 
 
